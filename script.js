3b2d6d87faf18d10f803111661ba7ec9d6d6bc78
andychan33 <55326812+andychan33@users.noreply.github.com>
2023-05-11 09:13:10 -0400

    set localStorage and added dropdown menu

--- script.js
+++ script.js
@@ -55,10 +55,7 @@ function initChart(target, data, labels) {
       return chart;
 }
 
-// helper to de-scope charts
-function transportChart(chart) {
-    return chart;
-}
+
 
 function processChartData(passedData) {
     const watershed_array = passedData.map(item => {
@@ -85,26 +82,35 @@ function processChartData(passedData) {
 }
 
 
+
+
 async function mainEvent() {
     const mainForm = document.querySelector('.main_form');
     const loadDataButton = document.querySelector('#data_load');
+    const clearDataButton = document.querySelector('#data_clear');
     const generateListButton = document.querySelector('#generate');
     const chart = document.querySelector('#myChart');
     const textField = document.querySelector('#destro');
+    const selectChoice = document.getElementById('select1');
+
+    const storedData = localStorage.getItem('storedData');
+    let parsedData = JSON.parse(storedData);
 
     let currentList = []; // scope is at the main event function level
-    //let descoped_newChart = null;
+    
     let newChart = null;
  
     loadDataButton.addEventListener('click', async(submitEvent) => {
         console.log('loading data');
 
         const results = await fetch('https://data.princegeorgescountymd.gov/resource/9tsa-iner.json');
-        currentList = await results.json(); 
-        console.log('table currentList');
-        console.table(currentList);
+        const storedList = await results.json();
+        localStorage.setItem('storedData', JSON.stringify(storedList));
+        parsedData = storedList;
+        //console.log('table currentList');
+        //console.table(currentList);
         
-        let chartData = processChartData(currentList);
+        let chartData = processChartData(storedList);
         newChart = initChart(chart, chartData[0], chartData[1]);
     })
 
@@ -112,19 +118,41 @@ async function mainEvent() {
 
     generateListButton.addEventListener('click', (event) => {
         console.log('generate new cleanup list');
-        const cleanupList = cutCleanupList(currentList);
+        const cleanupList = cutCleanupList(parsedData);
         injectHTML(cleanupList);
+        newChart.destroy()
+        let gChartData = processChartData(cleanupList);
+        newChart = initChart(chart, gChartData[0], gChartData[1]);
     })
 
     textField.addEventListener('input', (event) => {
         console.log('input', event.target.value);
-        const newList = filterList(currentList, event.target.value);
+        const newList = filterList(parsedData, event.target.value);
         console.log(newList);
         console.table(newList);
         injectHTML(newList);
         newChart.destroy()
         let fChartData = processChartData(newList);
         newChart = initChart(chart, fChartData[0], fChartData[1]);
+    })
+
+    selectChoice.addEventListener('click', (event) => {
+        console.log('select option is...', event.target.value);
+        const selectNewList = filterList(parsedData, event.target.value);
+        console.log(selectNewList);
+        console.table(selectNewList);
+        injectHTML(selectNewList);
+        newChart.destroy()
+        let sChartData = processChartData(selectNewList);
+        newChart = initChart(chart, sChartData[0], sChartData[1]);
+    })
+
+  
+
+    clearDataButton.addEventListener('click', (event) => {
+        console.log('clear browser data');
+        localStorage.clear();
+        console.log('localStorage check', localStorage.getItem("storedData"));
     })
 }
 
